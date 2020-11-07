0a447d12086fcb356f78ebac6deb40e84b63e7ef
YaroslavSulyma <45691318+YaroslavSulyma@users.noreply.github.com>
2020-11-07 21:09:01 +0200

    *created model class

---
+++ app/src/main/java/com/example/movieapp/models/MovieModel.java
@@ -0,0 +1,82 @@
+package com.example.movieapp.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MovieModel implements Parcelable {
+    private String title;
+    private String poster_path;
+    private String release_date;
+    private int movie_id;
+    private float vote_average;
+    private String movie_overview;
+
+    public MovieModel(String title, String poster_path, String release_date, int movie_id, float vote_average, String movie_overview) {
+        this.title = title;
+        this.poster_path = poster_path;
+        this.release_date = release_date;
+        this.movie_id = movie_id;
+        this.vote_average = vote_average;
+        this.movie_overview = movie_overview;
+    }
+
+    protected MovieModel(Parcel in) {
+        title = in.readString();
+        poster_path = in.readString();
+        release_date = in.readString();
+        movie_id = in.readInt();
+        vote_average = in.readFloat();
+        movie_overview = in.readString();
+    }
+
+    public static final Creator<MovieModel> CREATOR = new Creator<MovieModel>() {
+        @Override
+        public MovieModel createFromParcel(Parcel in) {
+            return new MovieModel(in);
+        }
+
+        @Override
+        public MovieModel[] newArray(int size) {
+            return new MovieModel[size];
+        }
+    };
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public int getMovie_id() {
+        return movie_id;
+    }
+
+    public float getVote_average() {
+        return vote_average;
+    }
+
+    public String getMovie_overview() {
+        return movie_overview;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(poster_path);
+        dest.writeString(release_date);
+        dest.writeInt(movie_id);
+        dest.writeFloat(vote_average);
+        dest.writeString(movie_overview);
+    }
+}
